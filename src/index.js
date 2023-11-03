27e8d3c9cd8e6f7100379465953318f4b6975d6e
JulioPaschoal <juliocpaschoal@gmail.com>
2023-11-03 09:19:09 -0300

    fix: corrigindo bug  na aplicação

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-// modules configuration \\
+// import module \\
 const express = require('express');
-const routes = require('./routes');
 const app = express();
 
-// Middleware configuration \\
-app.use(express.json());
-
-// Application routes \\
-app.use(routes);
+// My routes \\
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 // Start the server \\
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server started at  http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 });
