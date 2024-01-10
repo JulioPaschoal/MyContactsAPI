c3860cf29de30758d62df9f30a33fa87510acf6a
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2024-01-10 14:38:50 -0300

    Implementando metodo FindAll , FindById e FindByEmail

--- src/app/respositories/ContactsRepository.js
+++ src/app/respositories/ContactsRepository.js
@@ -20,24 +20,26 @@ let contacts = [
 const db = require('../../database');
 class ContactsRepository {
     // LISTAR TODOS OS REGISTRO \\
-    findAll() {
-        return new Promise(resolve => {
-            resolve(contacts);
-        });
+    async findAll() {
+        const rows = await db.query('SELECT * FROM contacts');
+        return rows;
     }
 
     // LISTAR UM REGISTRO \\
-    findById(id) {
-        return new Promise(resolve => {
-            resolve(contacts.find(contact => contact.id === id));
-        });
+    async findById(id) {
+        const [row] = await db.query('SELECT * FROM contacts WHERE id = $1', [
+            id,
+        ]);
+        return row;
     }
 
     // VERIFICAR EMAIL EXISTENTE\\
-    findByEmail(email) {
-        return new Promise(resolve => {
-            resolve(contacts.find(contact => contact.email === email));
-        });
+    async findByEmail(email) {
+        const [row] = await db.query(
+            'SELECT * FROM contacts WHERE email = $1',
+            [email],
+        );
+        return row;
     }
 
     // CRIAR UM REGISTRO \\
@@ -77,3 +79,5 @@ class ContactsRepository {
 }
 
 module.exports = new ContactsRepository();
+
+//AULA F039
